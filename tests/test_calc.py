811dd0d88faca1bc66e428ee4329ae1453bc74b6
t.ito <t.itou@churadata.okinawa>
2023-04-14 18:26:56 +0900

    kk

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,7 +5,7 @@ class TestCalculator(unittest.TestCase):
 
 	def test_add(self):
 		self.assertEqual(add(1, 2), 3)
-		elf.assertEqual(add(-1, 1), 0)
+		self.assertEqual(add(-1, 1), 0)
 		self.assertEqual(add(-1, -1), -2)
 
 	def test_subtract(self):
